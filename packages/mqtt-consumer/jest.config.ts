6115615ff995e22138e23f4ea72324479a090223
galah92 <galaharoni92@gmail.com>
2022-11-01 16:26:33 -0400

    Nx packages renaming

--- packages/mqtt-consumer/jest.config.ts
+++ packages/mqtt-consumer/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'consumer',
+  displayName: 'mqtt-consumer',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/consumer',
+  coverageDirectory: '../../coverage/packages/mqtt-consumer',
 };
