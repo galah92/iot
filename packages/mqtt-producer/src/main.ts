f35c45ea4ee608c8f61b68055a1b182edd47d583
galah92 <galaharoni92@gmail.com>
2022-11-02 13:12:59 -0400

    added prometheus & grafana for RabbitMQ monitoring

--- packages/mqtt-producer/src/main.ts
+++ packages/mqtt-producer/src/main.ts
@@ -9,12 +9,12 @@ import * as mqtt from 'async-mqtt';
     console.log('CONNECTED');
     let i = 0;
     /* eslint-disable no-constant-condition */
-    while (i < 3) {
+    while (true) {
       console.log(i);
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      const queue1 = 'bla/telemetry/events';
+      const queue1 = 'devices/id_abc/telemetry/events';
       await client.publish(queue1, `Hello RabbitMQ (${i})`);
-      const queue2 = 'bla/telemetry/events';
+      const queue2 = 'devices/id_def/telemetry/events';
       await client.publish(queue2, `Hello RabbitMQ (${i})`);
       i++;
     }
